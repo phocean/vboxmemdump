9e40092c7681da0a23c458b140fe897fab11beab
Jean-Christophe Baptiste <jc.baptiste@sysdream.com>
2018-10-02 15:49:04 +0200

    fix typo

--- vboxmemdump.sh
+++ vboxmemdump.sh
@@ -7,7 +7,7 @@ PATH=$(/usr/bin/getconf PATH)
 
 if [[ ! $1 ]];
 then
-  echo -e "\n[!] Please run '$0 <VM name>'\n"
+  echo -e "\n[!] Please run '$0 <VM name>\n"
   exit 1
 fi
 
@@ -17,16 +17,14 @@ FILENAME=$1-$DATE
 echo -e "\n[+] Dumping memory to file:\t$FILENAME.elf"
 VBoxManage debugvm $1 dumpvmcore --filename=$FILENAME.elf
 
-SIZE=0x$(objdump -h $FILENAME.elf | awk '/load1/ {print $3}')
-OFFSET=0x$(objdump -h $FILENAME.elf | awk '/load1/ {print $6}')
+SIZE=0x$(objdump -h $FILENAME.elf | awk '/\sload1\s/ {print $3}')
+OFFSET=0x$(objdump -h $FILENAME.elf | awk '/\sload1\s/ {print $6}')
 echo -e "[+] Raw memory offset:\t$OFFSET"
 echo -e "[+] Raw memory size:\t$SIZE"
 
-echo -e "[+] Extracting raw memory to file: $1-$DATE.raw"
+echo -e "[+] Extracting raw memory to file: $FILENAME.raw"
 head -c $(($SIZE+$OFFSET)) $FILENAME.elf|tail -c +$(($OFFSET+1)) > $FILENAME.raw
-
 echo -e "[+] Cleaning up."
 rm $FILENAME.elf
 echo -e "[+] Done, exiting."
-
 exit 0
