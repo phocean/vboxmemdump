57f6e3baa6c55de23b248787dbda785adcf019ba
Jean-Christophe Baptiste <jc.baptiste@sysdream.com>
2018-10-02 16:09:59 +0200

    Fix name with spaces

--- vboxmemdump.sh
+++ vboxmemdump.sh
@@ -15,16 +15,16 @@ DATE=$(date --iso-8601=seconds)
 FILENAME=$1-$DATE
 
 echo -e "\n[+] Dumping memory to file:\t$FILENAME.elf"
-VBoxManage debugvm $1 dumpvmcore --filename=$FILENAME.elf
+VBoxManage debugvm "$1" dumpvmcore --filename="$FILENAME".elf
 
-SIZE=0x$(objdump -h $FILENAME.elf | awk '/\sload1\s/ {print $3}')
-OFFSET=0x$(objdump -h $FILENAME.elf | awk '/\sload1\s/ {print $6}')
+SIZE=0x$(objdump -h "$FILENAME".elf | awk '/\sload1\s/ {print $3}')
+OFFSET=0x$(objdump -h "$FILENAME".elf | awk '/\sload1\s/ {print $6}')
 echo -e "[+] Raw memory offset:\t$OFFSET"
 echo -e "[+] Raw memory size:\t$SIZE"
 
-echo -e "[+] Extracting raw memory to file: $FILENAME.raw"
-head -c $(($SIZE+$OFFSET)) $FILENAME.elf|tail -c +$(($OFFSET+1)) > $FILENAME.raw
+echo -e "[+] Extracting raw memory to file: \"$FILENAME\".raw"
+head -c $(($SIZE+$OFFSET)) "$FILENAME".elf|tail -c +$(($OFFSET+1)) > "$FILENAME".raw
 echo -e "[+] Cleaning up."
-rm $FILENAME.elf
+rm "$FILENAME".elf
 echo -e "[+] Done, exiting."
 exit 0
